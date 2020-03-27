fda2b78e48270b576e738081a4ca2486e2b65e36
dede.exe <dede.exe@gmail.com>
2020-03-26 21:16:37 -0300

    remove: all

--- DDXNetwork/Source/RequestModel.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public protocol RequestModel {
-    var url: String { get }
-    var method: HttpMethod { get }
-    var body: HttpBody? { get }
-    var headers: HttpHeaders { get set }
-    var timeout: TimeInterval { get }
-}
